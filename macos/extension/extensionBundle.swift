03d70755fdf3c29cb90692682558efe0330c60f7
Abhishek Doshi <adoshi26.ad@gmail.com>
2026-02-18 16:26:07 +0530

    feat: Implement macOS widget and tray management, and update application icons.

---
+++ macos/extension/extensionBundle.swift
@@ -0,0 +1,17 @@
+//
+//  extensionBundle.swift
+//  extension
+//
+//  Created by Abhishek Doshi on 18/02/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct extensionBundle: WidgetBundle {
+    var body: some Widget {
+        extension()
+        extensionControl()
+    }
+}
